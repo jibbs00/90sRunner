5a9ae88266f6a6a696291c27a003439deba9d923
Michael Hnatiw <hnatiwm4@gmail.cm>
2013-06-20 18:05:38 -0400

    something something

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,15 +3,34 @@
      	  
      </div>
      <div id="left" class="column leftcol-inner-shading">
-	   
+         <h3>Interets</h3>
+         <form name="left-col-form">
+             <div class="input-append">
+                 <input name="left-input" class="input_ctm_sml" type="text" placeholder="...">
+		 <!-- Input and Actions to edit Interests -->
+		 <div class="btn-group">
+		      <button class="btn" tabindex="-1">Action</button>
+		      <button class="btn dropdown-toggle" data-toggle="dropdown" tabindex="-1">
+		         <span class="caret"></span>
+		      </button>
+  		      <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+		      	  <li><a tabindex="-1" href="#">Action</a></li>
+    		      	  <li><a tabindex="-1" href="#">Another action</a></li>
+   		      	  <li><a tabindex="-1" href="#">Something else here</a></li>
+    		      	  <li class="divider"></li>
+    		      	  <li><a tabindex="-1" href="#">Separated link</a></li>
+  		      </ul>
+		 </div>
+             </div>
+          </form>
      </div>
      <div id="right" class="column rightcol-inner-shading">
 
      	  <h3>Add Content!</h3>
 	  <!-- action loads defined controller -->
-	  <form name="site_form" class="form-inline" method="post" action="">
+	  <form name="right-col-form" class="form-inline" method="post" action="">
 	  	<div class="input-append">
-		     <input name="site_input" class="input-rightcol" type="text" placeholder="add URL">
+		     <input name="site_input" class="input_ctm_sml" type="text" placeholder="add URL">
 		     <button name="add_button" class="btn" type="submit">Add</button>
 		</div>
 	  </form>
